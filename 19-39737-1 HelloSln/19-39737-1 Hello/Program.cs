e3a622cda5f76cc3f35c683952dda681eabf05f2
Sreejan Roy <sreejanroy01@gmail.com>
2021-02-01 10:25:38 +0600

    19-39737-1 Hello World

---
+++ 19-39737-1 HelloSln/19-39737-1 Hello/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_39737_1_Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
