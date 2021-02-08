2566a9d526348a751a5f76baab652a1b29beb38c
Sreejan Roy <sreejanroy01@gmail.com>
2021-02-08 10:42:36 +0600

    Lab_Task_3

---
+++ Lab_Task_3Sln/Lab_Task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab_Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Sreejan Roy", "19-39737-1");
+            s1.ShowInfo();
+            s1.Name = "Sreejan Roy";
+            s1.Id = "19-39737-1";
+            s1.Department = "CSE";
+            s1.Cgpa = 3.47f;
+            Console.WriteLine("Name: " + s1.Name);
+            Console.WriteLine("Id: " + s1.Id);
+            Console.WriteLine("Department: " + s1.Department);
+            Console.WriteLine("Cgpa: " + s1.Cgpa);
+        }
+    }
+}
---
+++ Lab_Task_3Sln/Lab_Task_3/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_Task_3
+{
+    public class Student
+    {
+        string name;
+        string id;
+        string department;
+        float cgpa;
+        //properties
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+
+        public string Id
+        {
+            set { id = value; }
+            get { return id; }
+        }
+        public string Department
+        {
+            set { department = value; }
+            get { return department; }
+        }
+
+        public float Cgpa
+        {
+            set { cgpa = value; }
+            get { return cgpa; }
+        }
+
+        //Constructor
+        public Student()
+        {
+            Console.WriteLine("Student created with default");
+        }
+        public Student(string name, string id)
+        {
+            Console.WriteLine("Student created with 2 parameter");
+            this.name = name;
+            this.id = id;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name is: " + name);
+            Console.WriteLine("Id is: " + id);
+            Console.WriteLine("Cgpa is: " + cgpa);
+        }
+    }
+}
