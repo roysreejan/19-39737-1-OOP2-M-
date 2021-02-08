b848fbdfafdc3821ad4ca85c5fbc3b220a0ce1e7
Sreejan Roy <sreejanroy01@gmail.com>
2021-02-08 10:08:13 +0600

    Lab2.1

---
+++ Lab2.1Sln/Lab2.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            int i;
+            Console.WriteLine("Even numbers: ");
+            {
+                for (i = 1; i <= n; i++)
+                {
+                    if (i% 2 == 0)
+                    {
+                        Console.Write(i + ", ");
+                    }
+                }
+            }
+            Console.WriteLine("");
+        }
+    }
+}
