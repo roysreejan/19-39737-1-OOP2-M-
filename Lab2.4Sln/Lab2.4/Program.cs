972d4b7caa4b0af05bea1733ed661b3de5b96b98
Sreejan Roy <sreejanroy01@gmail.com>
2021-02-08 10:17:37 +0600

    Lab2.4

---
+++ Lab2.4Sln/Lab2.4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int SumofEven = 0, SumofOdd = 0;
+            for(int i= 1; i<=100; i++)
+            {
+                if(i%2==0)
+                {
+                    SumofEven = SumofEven + i;
+                }
+                else
+                {
+                    SumofOdd = SumofOdd + i;
+                }
+            }
+            Console.WriteLine("Sum of Even Number from 1 to 100: " + SumofEven);
+            Console.WriteLine("Sum of Odd Number from 1 to 100: " + SumofOdd);
+        }
+    }
+}
