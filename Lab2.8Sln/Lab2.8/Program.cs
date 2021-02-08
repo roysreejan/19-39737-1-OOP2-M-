dd516b680c4f338fbc39ac801e782286d7d8a5d9
Sreejan Roy <sreejanroy01@gmail.com>
2021-02-08 10:24:16 +0600

    Lab2.8

---
+++ Lab2.8Sln/Lab2.8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Lab2._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double phy= 92, chem= 85.7, bio= 85.5, math= 95, computer= 95, sum= 0.0;
+
+            sum = phy+chem+bio+math+computer;
+            sum = (100 * sum) / 500;
+
+            if (sum >= 90)
+            {
+                Console.WriteLine("Grade: A+");
+            }
+            else if (sum >= 85 && sum < 90)
+            {
+                Console.WriteLine("Grade: A");
+            }
+            else if (sum >= 80 && sum < 85)
+            {
+                Console.WriteLine("Grade: B+");
+            }
+            else if (sum >= 75 && sum < 80)
+            {
+                Console.WriteLine("Grade: B");
+            }
+            else if (sum >= 50 && sum < 75)
+            {
+                Console.WriteLine("Grade: C+");
+            }
+            else
+            {
+                Console.WriteLine("Grade: F");
+            }
+
+            Console.WriteLine("");
+        }
+    }
+}
