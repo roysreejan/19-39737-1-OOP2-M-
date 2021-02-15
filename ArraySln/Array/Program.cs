3c4ce1ecda884b50c4db235d2a02e04852cc9757
Sreejan Roy <sreejanroy01@gmail.com>
2021-02-15 15:08:00 +0600

    Array update

--- ArraySln/Array/Program.cs
+++ ArraySln/Array/Program.cs
@@ -10,37 +10,45 @@ namespace Array
     {
         static void Main(string[] args)
         {
+            int i, j, k;
             //read n number and display reverse order
             int[] array = new int[] { 1, 2, 3, 4, 5 };
             Console.WriteLine("Array in reverse order: ");
 
-            for (int i = array.Length - 1; i >= 0; i--)
+            for (i = array.Length - 1; i >= 0; i--)
             {
                 Console.WriteLine(array[i]);
             }
-           Console.WriteLine();
+            Console.WriteLine();
 
 
             //sum of all elements of the array
             int[] arr = new int[] { 1, 2, 3, 4, 5 };
             int sum = 0;
-            for (int i = 0; i < arr.Length; i++)
+            for (i = 0; i < arr.Length; i++)
             {
                 sum += arr[i];
             }
-            Console.WriteLine("The sum of all elements of the array: {0}" , sum);
+            Console.WriteLine("The sum of all elements of the array: {0}", sum);
             Console.WriteLine();
 
 
             //copy to another array
             int[] arr1 = new int[] { 1, 2, 3, 4, 5 };
             int[] arr2 = new int[arr1.Length];
-            for (int i = 0; i < arr1.Length; i++)
+            for (i = 0; i < arr1.Length; i++)
             {
                 arr2[i] = arr1[i];
             }
+            Console.WriteLine("Elements of original array: ");
+            for (i = 0; i < arr1.Length; i++)
+            {
+                Console.Write(arr1[i] + " ");
+            }
+            Console.WriteLine();
+
             Console.WriteLine("Elements of new array: ");
-            for (int i = 0; i < arr2.Length; i++)
+            for (i = 0; i < arr2.Length; i++)
             {
                 Console.Write(arr2[i] + " ");
             }
@@ -48,27 +56,45 @@ namespace Array
 
 
             //total number of duplicate value
-            int[] arr3 = {5, 3, 5, 8, 6, 3, 9, 3 };
-            
-            for (int i = 0; i < arr3.Length; i++)
+            int[] arr3 = new int[] { 5, 3, 5, 8, 6, 3, 9, 3 };
+
+            for (i = 0; i < arr3.Length; i++)
             {
                 int count = 0;
-                for (int j = i; j < arr3.Length; j++)
+                for (j = i; j < arr3.Length; j++)
                 {
 
                     if (arr3[i] == arr3[j])
                     {
-                        count=count+1;
+                        count = count + 1;
                     }
                 }
                 Console.WriteLine();
-                Console.WriteLine("Total number of duplicate elements in an array:" + count);
+                Console.WriteLine(  arr3[i] + " duplicate occurse {0} time",  count );
             }
 
 
             //print all unique elements in an array
-            
+            for (i = 0; i < arr3.Length; i++)
+            {
+                int Unique = 0;
+                for (j = 0, k = arr3.Length; j < k + 1; j++)
+                {
 
+                    if (i != j)
+                    {
+                        if (arr3[i] == arr3[j])
+                        {
+                            Unique = Unique + 1;
+                        }
+                    }
+                }
+                if (Unique == 0)
+                {
+                    Console.Write(" {0}  ", arr3[i]);
+                }
+                Console.WriteLine("Total number of unique elements in an array: {0}", Unique);
+            }
         }
     }
 }
